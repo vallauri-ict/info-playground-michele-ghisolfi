df64fc7c2aa803216e82497115849a0a4370470e
michele-ghisolfi <62621504+michele-ghisolfi@users.noreply.github.com>
2020-11-13 09:58:37 +0100

    Create 011

---
+++ 011_Lista/011_Lista/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Lista
+{
+    class Books
+    {
+        public string autore;
+        public string titolo;
+        public int id;
+
+        public Books()
+        {
+
+        }
+    }
+}
---
+++ 011_Lista/011_Lista/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _011_Lista
+{
+    public partial class Form1 : Form
+    {
+        public struct Libro
+        {
+            public int ID;
+            public string Titolo;
+            public string Autore;
+        }
+
+        List<Libro> miaLista = new List<Libro>();
+        List<Books> listaLibri = new List<Books>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Libro l;
+            l.ID = Convert.ToInt32(txtId.Text);
+            l.Titolo = txtTitolo.Text;
+            l.Autore = txtAutore.Text;
+
+            miaLista.Add(l);
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (var elemento in miaLista)
+            {
+                s += elemento.Titolo + "\n";  
+            }
+            MessageBox.Show(s);
+        }
+
+        private void btnAggiungiObj_Click(object sender, EventArgs e)
+        {
+            Books b = new Books();
+            b.autore = txtAutore.Text;
+            b.id = Convert.ToInt32(txtId.Text);
+            b.titolo = txtTitolo.Text;
+
+            listaLibri.Add(b);
+        }
+
+        private void btnVisualizzaObj_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (Books item in listaLibri)
+            {
+                s += item.titolo + "\n";
+            }
+            MessageBox.Show(s);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
