aa753ff8a32049628261ee7834b1177909b1ac38
unknown <micheleghis@gmail.com>
2020-12-11 09:57:30 +0100

    Create 018

---
+++ 018_OOP-PersoneStudenti/018_OOP-PersoneStudenti/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018_OOP_PersoneStudenti
+{
+    class Persona
+    {
+        private string nome, cognome, sesso;
+        private int eta;
+
+        public void SetAttributs(string nome, string cognome, string sesso, int eta)
+        {
+            if (true)
+            {
+                // controllo tutti valori ok
+            }
+            this.nome = nome;
+            this.cognome = cognome;
+            this.sesso = sesso;
+            this.eta = eta;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetCognome()
+        {
+            return this.cognome;
+        }
+
+        public string GetSesso()
+        {
+            return this.sesso;
+        }
+
+        public int Geteta()
+        {
+            return this.eta;
+        }
+    }
+}
