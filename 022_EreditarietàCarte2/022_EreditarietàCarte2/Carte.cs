c19fd4b5dcf3df70766d824b62a0eec747afaa6c
unknown <micheleghis@gmail.com>
2021-02-10 09:04:24 +0100

    Create ex 022

---
+++ 022_EreditarietàCarte2/022_EreditarietàCarte2/Carte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _022_EreditarietàCarte2
+{
+    class Carte
+    {
+        public string Valore { get; set; }
+        public string Seme { get; set; }
+
+        public string[] listaSemi = { "Cuore", "Picche", "Fiore", "Quadri" };
+        public string[] listaVal = { "Asso", "2", "3", "4", "5", "6", "7", "Fante", "Regina", "Re" };
+
+        public Carte(string valore, string seme)
+        {
+            Valore = valore;
+            Seme = seme;
+        }
+    }
+}
