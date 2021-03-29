5c816098618a740d0e203a01ea705546f6f54b8f
unknown <micheleghis@gmail.com>
2021-03-29 10:43:10 +0200

    Create 029

---
+++ 029_Delegate-DispenseMana/029_Delegate-DispenseMana/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace _029_Delegate_DispenseMana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalva = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalva
+            // 
+            this.btnSalva.Location = new System.Drawing.Point(106, 61);
+            this.btnSalva.Name = "btnSalva";
+            this.btnSalva.Size = new System.Drawing.Size(81, 29);
+            this.btnSalva.TabIndex = 0;
+            this.btnSalva.Text = "Salva";
+            this.btnSalva.UseVisualStyleBackColor = true;
+            this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 149);
+            this.Controls.Add(this.btnSalva);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalva;
+    }
+}
+
---
+++ 029_Delegate-DispenseMana/029_Delegate-DispenseMana/GestoreSalvataggio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _029_Delegate_DispenseMana
+{
+    // DICHIARO il mio delegato
+    public delegate void salvataggioEseguitoEventHandler(int id);
+
+    class GestoreSalvataggio
+    {
+        // dichiaro l'evento
+        public event salvataggioEseguitoEventHandler salvataggioEseguito;
+
+        public void salvaSuFile(int id)
+        {
+            MessageBox.Show("Sto eseguendo il salvataggio");
+
+            salvataggioEseguito(id);
+        }
+
+
+    }
+}
