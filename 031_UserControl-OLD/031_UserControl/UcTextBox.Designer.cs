95194497e032ff2fe487059bef82cc2453ca23df
unknown <micheleghis@gmail.com>
2021-03-29 10:44:10 +0200

    Create 031-OLD

---
+++ 031_UserControl-OLD/031_UserControl/UcTextBox.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace _031_UserControl
+{
+    partial class UcTextBox
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTesto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtTesto
+            // 
+            this.txtTesto.Location = new System.Drawing.Point(0, 0);
+            this.txtTesto.Name = "txtTesto";
+            this.txtTesto.Size = new System.Drawing.Size(197, 20);
+            this.txtTesto.TabIndex = 0;
+            this.txtTesto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTesto_KeyPress);
+            // 
+            // UcTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtTesto);
+            this.Name = "UcTextBox";
+            this.Size = new System.Drawing.Size(197, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTesto;
+    }
+}
---
+++ 031_UserControl-OLD/031_UserControl/UcTextBox.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _031_UserControl
+{
+    public partial class UcTextBox : UserControl
+    {
+        private bool numero = false;    // se true la textBox acceta solo numeri
+        private string testo;           // testo della textbox
+        private int cifreDecimali = 0;  // contiene il numero di cifre decimali (dopo la virgola)
+
+        public UcTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public bool Numero { get => numero; set => numero = value; }
+        public string Testo { get => testo = txtTesto.Text; set => txtTesto.Text = testo = value; }
+        public int CifreDecimali
+        {
+            get => cifreDecimali;
+            set
+            {
+                cifreDecimali = value;
+                reimpostaTesto();
+            }
+        }
+        
+
+        private void reimpostaTesto()
+        {
+            if (numero && Testo!="")
+            {
+                try
+                {
+                    // controllare che non ci siano + di una virgola
+                    if (ContaVirgola(Testo) > 1)
+                    {
+                        throw new Exception("Ci sono troppa virgole");
+                    }
+
+                    // controllo dei decimali
+                    double numero = Math.Round(Convert.ToDouble(Testo), CifreDecimali);
+                    Testo = numero.ToString();
+
+                    // Gestione degli 00 dopo la virgola in caso di numero intero
+
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Valore non valido");
+                }
+            }
+        }
+
+        private int ContaVirgola(string t)
+        {
+            int count = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                char c = Convert.ToChar(t.Substring(i, 1));
+                if (c == ',')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public void Pulisci()
+        {
+            Testo = "";
+        }
+
+        private void txtTesto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        /*
+        private void TxtTesto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (numero)
+            {                               // char.IsPunctuation()
+                if (!(char.IsDigit(e.KeyChar) || e.KeyChar == ',' || char.IsControl(e.KeyChar)))
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }*/
+    }
+}
